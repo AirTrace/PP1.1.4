8f85819466833c2f814b006b781a2c4f4a143904
AirTrace <silvertracework@.gmail.com>
2022-02-21 13:15:52 +0300

    work

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,4 +1,5 @@
 package jm.task.core.jdbc;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
@@ -9,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("Ivan", "Ivanov", (byte) 20);
-        userService.saveUser("Dima", "German", (byte) 22);
-        userService.saveUser("Petya", "Alexandrov", (byte) 23);
-        userService.saveUser("Alexandr", "Petrovich", (byte) 31);
+        userService.saveUser("Dim", "Yag", (byte)23);
+        userService.saveUser("Sasha", "Meg", (byte)25);
+        userService.saveUser("Ivan", "Ivanov", (byte)20);
+        userService.saveUser("Alexandr", "Ser", (byte)33);
         List<User> list = userService.getAllUsers();
         for (User user : list) {
             System.out.println(user);
